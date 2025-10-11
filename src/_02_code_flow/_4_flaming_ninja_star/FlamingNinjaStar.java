3fd9283cd57c0cf3ca07c3b0be1e604f9fc48913
league <league@wts14-3.attlocal.net>
2025-10-11 12:59:10 -0700

    6767

--- src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
+++ src/_02_code_flow/_4_flaming_ninja_star/FlamingNinjaStar.java
@@ -10,39 +10,107 @@ public class FlamingNinjaStar {
 		int flameSize = 130;		//the length of the flaming arms
 		
 		// Make a new robot, and set it's pen down.
+		Robot mason = new Robot(); 
+		mason.penDown();
+		mason.setPenColor(222, 95, 11);
 
 		// Set the robot speed to 100
+		mason.setSpeed(100);
 		
 		// Set the robot window size to 800 x 800
+		mason.setWindowSize(800,800);
 
 		//  COUNT. Make another int variable to count how many times the loop has repeated
 		//         Set its start value to zero.
-		
+		int count = 0;
 	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
-
+		while (count<150) {
+				   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
+			mason.setPenColor(0, 0, 0);
+			mason.turn(360/8);
+				   // MOVE           Move the robot 64 pixels
+			mason.move(64);
+	
+				   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			mason.turn(-40);	
+				   // DRAW FLAME     Move the robot the distance in the variable flameSize
+			mason.setPenColor(222, 95, 11);
+			mason.move(flameSize);	
+					//               Turn the robot 170 degrees
+			mason.turn(170);
+			
+					//               Move the robot the distance in the variable flameSize (again)
+			mason.move(flameSize);	
+			mason.setPenColor(0, 0, 0);
+				   // TURN RIGHT     Turn the robot 64 degrees to the right
+			mason.turn(64);
+					// MOVE         Move the robot the distance in the variable baseSize
+			mason.move(baseSize);
+					//  INCREASE COUNT. Increase the count by 1
+			count += 1;
+			//  End the while loop here
+		}
+		mason.move(400);
+		int yes = 0;
+	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
+		while (yes<150) {
 			   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
-		
+		mason.setPenColor(0, 0, 0);
+		mason.turn(360/8);
 			   // MOVE           Move the robot 64 pixels
+		mason.move(64);
 
 			   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
-			
+		mason.turn(-40);	
 			   // DRAW FLAME     Move the robot the distance in the variable flameSize
-			
+		mason.setPenColor(222, 95, 11);
+		mason.move(flameSize);	
 				//               Turn the robot 170 degrees
-			
+		mason.turn(170);
+		
 				//               Move the robot the distance in the variable flameSize (again)
-			
+		mason.move(flameSize);	
+		mason.setPenColor(0, 0, 0);
 			   // TURN RIGHT     Turn the robot 64 degrees to the right
-			
+		mason.turn(64);
 				// MOVE         Move the robot the distance in the variable baseSize
-		
+		mason.move(baseSize);
 				//  INCREASE COUNT. Increase the count by 1
-		
+		yes += 1;
 		//  End the while loop here
-		
+		}
 		// TEST   Run the program. Check that your shape is the same as the first picture in the recipe. 
 		//        This is one arm of the ninja star.
-		
+		mason.turn(20);
+		mason.move(400);
+		int wow = 0;
+	       //  LOOP. Start a while loop to repeat all of the code below ONE time (we will change this later)
+		while (wow<150) {
+				   // TURN RIGHT     Turn the robot 1/8 of a circle (hint: 360 degrees will turn a full circle)
+			mason.setPenColor(0, 0, 0);
+			mason.turn(360/8);
+				   // MOVE           Move the robot 64 pixels
+			mason.move(64);
+	
+				   // TURN LEFT      Turn the robot 40 degrees to the LEFT. (Negative numbers will turn the robot counter-clockwise.)
+			mason.turn(-40);	
+				   // DRAW FLAME     Move the robot the distance in the variable flameSize
+			mason.setPenColor(222, 95, 11);
+			mason.move(flameSize);	
+					//               Turn the robot 170 degrees
+			mason.turn(170);
+			
+					//               Move the robot the distance in the variable flameSize (again)
+			mason.move(flameSize);	
+			mason.setPenColor(0, 0, 0);
+				   // TURN RIGHT     Turn the robot 64 degrees to the right
+			mason.turn(64);
+					// MOVE         Move the robot the distance in the variable baseSize
+			mason.move(baseSize);
+					//  INCREASE COUNT. Increase the count by 1
+			wow += 1;
+			//  End the while loop here
+		}
 		// COLOR  Change the Robot's pen color so that the flame is a different color to the rest of the star.
 		//        Run the program again. Check the second picture in the recipe.
 
